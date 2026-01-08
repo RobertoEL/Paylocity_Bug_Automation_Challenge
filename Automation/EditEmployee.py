c6ad0abc0027b2638273b23cbf4d625c22ea3751
Roberto Esparza <phd.roberto.esparza@gmail.com>
2026-01-07 19:16:42 -0600

    Create EditEmployee.py

---
+++ Automation/EditEmployee.py
@@ -0,0 +1,57 @@
+import string
+
+from selenium import webdriver
+from Utils import *
+import random
+
+# === Config ===
+LOGIN_URL = "https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login"
+USERNAME = "TestUser854"
+PASSWORD = "n7{A1l)0NM!u"
+FIRST_NAME = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+LAST_NAME = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+DEPENDANTS = rand_int = random.randint(0, 32)
+NEW_FIRST_NAME = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+NEW_LAST_NAME = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+NEW_DEPENDANTS = rand_int = random.randint(0, 32)
+
+# === Start Browser ===
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+try:
+    # Login
+    login(driver, LOGIN_URL, USERNAME, PASSWORD)
+
+except Exception as e:
+    print("Login failed:", e)
+
+try:
+    # Add an employee
+    add_employee(driver, FIRST_NAME, LAST_NAME, DEPENDANTS)
+
+    # Verify the new employee entry
+    if employee_exists(driver, FIRST_NAME, LAST_NAME, DEPENDANTS):
+        print("Employee added successfully.")
+    else:
+        print("Employee not found.")
+
+except Exception as e:
+    print("Error adding the employee:", e)
+
+try:
+    # Edit the employee
+    edit_employee(driver, FIRST_NAME, LAST_NAME, DEPENDANTS, NEW_FIRST_NAME, NEW_LAST_NAME, NEW_DEPENDANTS)
+
+    # Verify the employee was edited
+    if employee_exists(driver, NEW_FIRST_NAME, NEW_LAST_NAME, NEW_DEPENDANTS) and not employee_exists(driver, FIRST_NAME, LAST_NAME, DEPENDANTS):
+        print("Employee edited successfully.")
+    else:
+        print("Employee was not edited.")
+
+except Exception as e:
+    print("Error editing the employee:", e)
+
+finally:
+    # Clean up
+    driver.quit()
